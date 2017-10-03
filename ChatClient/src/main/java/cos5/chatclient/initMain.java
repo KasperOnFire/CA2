4af92be85c12fd0d7c3960f69797b08898f3d476
CaptainVillain <kristian.krog@gmail.com>
2017-10-03 15:37:26 +0200

    Now is web!

--- ChatClient/src/main/java/cos5/chatclient/initMain.java
+++ ChatClient/src/main/java/cos5/chatclient/initMain.java
@@ -7,7 +7,9 @@ package cos5.chatclient;
 public class initMain {
 
     public static void main(String[] args) {
-        
+        String string = "asj:asjdaj:dajsd";
+        String[] split = string.split(":");
+        System.out.println(split.toString());
     }
 
 }
